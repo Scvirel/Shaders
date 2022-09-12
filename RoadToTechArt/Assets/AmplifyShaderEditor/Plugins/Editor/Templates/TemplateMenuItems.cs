e1c51e6bb4b73cbd8fdc5b691c96845f4e052af3
Vladislav <singayevskyvladislav@gmail.com>
2022-09-12 23:13:18 +0300

    Metalic shader + 3 material templates

--- RoadToTechArt/Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
+++ RoadToTechArt/Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
@@ -56,6 +56,11 @@ namespace AmplifyShaderEditor
 		{
 			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "e1de45c0d41f68c41b2cc20c8b9c05ef" );
 		}
+		[MenuItem( "Assets/Create/Amplify Shader/Legacy/Samples/DoublePassUnlit", false, 85 )]
+		public static void ApplyTemplateLegacySamplesDoublePassUnlit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "003dfa9c16768d048b74f75c088119d8" );
+		}
 		[MenuItem( "Assets/Create/Amplify Shader/Legacy/Lit", false, 85 )]
 		public static void ApplyTemplateLegacyLit()
 		{
